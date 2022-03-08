bc68cc18c0a85f7d442a77e406da22d42363f550
bidope <85484224+bdp2732@users.noreply.github.com>
2022-03-08 14:58:18 +0900

    Update monster.cpp

--- monster.cpp
+++ monster.cpp
@@ -13,23 +13,23 @@ int main()
     double ball_r;
     double i;
     double g = -9.8;
-    int a =0;
+    double a;
+
     while(1){
         cout << "초기 속도 :";
-3        cin >> vel;
+        cin >> vel;
         cout << "angle:";
         cin >> angle;
         cout << "진행 시간";
         cin >> a;
         for(i=0; i<=a; i+=0.1 ){
-            ball_h = i*vel*cos(angle*pi/180);
-            ball_r = i*vel*sin(angle*pi/180)-(1/2)*g*i*i;
-            if(ball_h<=0){
-                continue;
-            } else if (ball_h<0){
-                ball_h=0;
-                continue;
+            ball_r = i*vel*cos(angle*pi/180);
+            ball_h = i*vel*sin(angle*pi/180)+0.5*g*i*i;
+            if (ball_h < 0){
+                ball_h = 0;
+                break;
             }
+
         }
 
         if(ball_h<=width && ball_r<=height){
